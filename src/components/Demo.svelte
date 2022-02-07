bc4fc62285fa28f717bdbbfa1574a53b654c26cf
David Fitzgibbon <davidfitzgibbon@gmail.com>
2022-02-07 21:22:31 +0000

    v0.1.1 docs

---
+++ src/components/Demo.svelte
@@ -0,0 +1,30 @@
+<script>
+  export let title = 'Demo';
+  export let url = "basic-threeasy-animation-onstv"
+</script>
+
+<h3>{title}</h3>
+<iframe class="demo-iframe" src="https://codesandbox.io/embed/{url}?fontsize=14&hidenavigation=1&module=%2Fapp.js&theme=dark&view=editor"
+     style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
+     title="Basic Threeasy"
+     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+   ></iframe>
+
+   <style>
+     h3 {
+       font-size: 14px;
+       font-weight: 200;
+      grid-column-start: 1;
+      grid-column-end: 4;
+      margin-top: 1em;
+      margin-bottom: .5em;
+     }
+     .demo-iframe {
+      width: 100%;
+      height: 400px;
+      grid-column-start: 1;
+      grid-column-end: 4;
+      margin-bottom: 2em;
+    }
+   </style>
---
+++ src/components/Logo.svelte
@@ -0,0 +1,27 @@
+<script>
+  const version = "0.1.1";
+</script>
+<div class="logo">
+<img src="/imgs/threeasy.png" alt="Threeasy">
+<div class="details">
+<h1>Threeasy</h1>
+<h2>v0.1.1</h2>
+</div>
+</div>
+<style>
+  .logo {
+    position: absolute;
+    top: 0;
+    left: 1rem;
+  }
+
+  .logo h1,
+  .logo h2 {
+    font-size: 15px;
+    line-height: 1.4;
+  }
+
+  img {
+    max-width: 75px;
+  }
+</style>
