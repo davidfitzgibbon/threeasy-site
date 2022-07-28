3cdb82ddd61694309a43af86918dd351df6b84f9
David Fitzgibbon <davidfitzgibbon@gmail.com>
2022-07-28 21:40:31 +0100

    examples to local

--- src/components/Demo.svelte
+++ src/components/Demo.svelte
@@ -7,27 +7,27 @@
 
 <slot />
 
-<iframe class="demo-iframe" src="https://codesandbox.io/embed/{url}?fontsize=14&hidenavigation=1&theme=dark&view=preview"
+<iframe class="demo-iframe" src="/examples/{url}"
      style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
-     title="Basic Threeasy"
+     title="{title}"
      allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
      sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
    ></iframe>
 
-   <style>
-     h3 {
-       font-size: 14px;
-       font-weight: 200;
-      grid-column-start: 1;
-      grid-column-end: 4;
-      margin-top: 1em;
-      margin-bottom: .5em;
-     }
-     .demo-iframe {
-      width: 100%;
-      height: 400px;
-      grid-column-start: 1;
-      grid-column-end: 4;
-      margin-bottom: 2em;
-    }
-   </style>
+<style>
+  h3 {
+    font-size: 14px;
+    font-weight: 200;
+  grid-column-start: 1;
+  grid-column-end: 4;
+  margin-top: 1em;
+  margin-bottom: .5em;
+  }
+  .demo-iframe {
+    width: 100%;
+    height: 400px;
+    grid-column-start: 1;
+    grid-column-end: 4;
+    margin-bottom: 2em;
+  }
+</style>
